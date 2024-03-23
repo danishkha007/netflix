bfbfd9d9a8fe2c6703d0f4396aaa595924374739
danishkha007 <73287649+danishkha007@users.noreply.github.com>
2024-03-23 15:09:05 +0000

    changed congigurations

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,10 @@
 <?php 
 
 define('DB_HOST','localhost');
-define('DB_NAME','netflix');
-define('DB_USER','root');
-define('DB_PASS','');
-define('WWW_ROOT','http://localhost/netflix/');
+define('DB_NAME','id22000329_net');
+define('DB_USER','id22000329_danish');
+define('DB_PASS','Danish@123');
+define('WWW_ROOT','https://databases.000webhost.com/');
 
 
 //SMTP
@@ -13,3 +13,4 @@ define("M_USERNAME","aktureference@gmail.com");
 define("M_PASSWORD","ljvxcyyudtftomys");
 define("M_SMTPSECURE","tls");
 define("M_PORT","587");
+?>
